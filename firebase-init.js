e5956826b6192884d781e98fbcdf7202e118d643
24-priya <priyakokate2018@gmail.com>
2025-06-07 14:48:34 +0530

    Add files via upload

---
+++ firebase-init.js
@@ -0,0 +1,12 @@
+
+// firebase-init.js
+const firebaseConfig = {
+   apiKey: "AIzaSyCN6u3rmAHNmOFp4I3WEEJUSatJozDVs_U",
+  authDomain: "ecommerce-website-a77a8.firebaseapp.com",
+  projectId: "ecommerce-website-a77a8",
+  storageBucket: "ecommerce-website-a77a8.appspot.com",
+  messagingSenderId: "234020577578",
+  appId: "1:234020577578:web:468c002a459edd59a0be53"
+};
+
+firebase.initializeApp(firebaseConfig);
